b3059e8a0dd36399e33133161559fd79b166ac09
israelrmartinez <israel.r.mar@gmail.com>
2023-06-14 18:06:02 -0700

    Revert "Revert "first commit to gh-pages branch" (#11)"
    This reverts commit 323566e197cade9580beb44905f0cbf1726a8fb5.

--- build/static/js/84.cfe1c4e7.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkismartinez=self.webpackChunkismartinez||[]).push([[84],{7084:function(e,a,i){i.r(a);i(2791),i(3508);var n=i(184);a.default=function(e){var a=e.title;return(0,n.jsx)("div",{className:"header",children:a})}}}]);
-//# sourceMappingURL=84.cfe1c4e7.chunk.js.map
